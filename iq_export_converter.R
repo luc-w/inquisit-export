cf59ced8ac55f1854955aab112eced765e24f5d5
luc-w <=>
2023-11-25 15:31:49 +0100

    fixed in bug in file naming for survey data

--- iq_export_converter.R
+++ iq_export_converter.R
@@ -1,22 +1,26 @@
 wd <- getwd() # get working directory
 dirs <- list.dirs() # get list of directories withing working directory
 
-for(i in 1:length(dirs)){
+for (i in 1:length(dirs)) {
   
   temp <- list.files(path = dirs[i] , pattern = "*.iqdat") # get list of .iqdat-files within the current directory
   
-  check_invalid <- identical(temp, character(0)) | # check if directory is empty (of .iqdat-files) or
-                    length(temp) == 1               # or if there is only a single files (which is likely some summary file)
+  valid <- identical(temp, character(0)) | # check if directory is empty (of .iqdat-files) or
+                     length(temp) == 1               # or if there is only a single files (which is likely some summary file)
   
   # if directory contains .iqdat files, join and export them as .csv-files
-  if(check_invalid == 0) {
+  if (valid == 0) {
     
   list_files <- lapply(paste0(dirs[i], "/", temp), read.delim, stringsAsFactors = F) # import all files in a list
   dat <- do.call(rbind, list_files) # bind elements of the file list to a data.frame
   
-  filename <- paste0(sub("/", "_", sub("./", "", dirs[i])), ".csv") # define file name for new data.frame
+    if (grepl("survey", dirs[i])){ # check if data stems from a "survey" (type of inquisit study)
+      filename <- paste0(sub("/", "_", sub("/", "_", sub("./", "", dirs[i]))), ".csv") # set file name for survey data
+    } else {
+      filename <- paste0(sub("/", "_", sub("./", "", dirs[i])), ".csv") # set file name for experiment data
+    }
+    
   write.csv2(dat, file = paste0(wd, "/", filename), row.names = FALSE) # export data.frame to working directory       
-        
   print(paste0("Files in", dirs[i], " joined and exported."))
   
   # if directory contains <= 2 .iqdat-files, print note and proceed with next directory
